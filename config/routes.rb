13ebbafbdcce3ea24be2979b6721ac891716c004
jhyun94 <hyunjonathan@gmail.com>
2017-02-24 11:42:02 -0600

    created homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root to: 'welcome#index'
+
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
