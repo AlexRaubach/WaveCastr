e24c1c03356d9f23427b34f51bc22d3202a3a89c
Oliver Duncan <oliverduncan@icloud.com>
2017-02-24 17:32:23 -0600

    Limit user routes to show

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   
   devise_for :users
-  resources :users
+  resources :users, only: [:show]
   resources :episodes, only: [:create, :show]
 
 end
