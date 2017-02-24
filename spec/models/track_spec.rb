3da5c7d9975e92fd0a3dd81bd82260aefa384713
Oliver Duncan <oliverduncan@icloud.com>
2017-02-24 15:51:53 -0600

    Configure track associations

--- spec/models/track_spec.rb
+++ spec/models/track_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Track, type: :model do
-  let(:track) { Track.new(recordable_id: 1, recordable_type: "User", s3_string: "secret") }
+  
+  let(:episode) { Episode.create(name: "My podcast", description: "My first one!") }
+  let(:guest) { episode.guests.new(name: "Oliver") }
+  let(:track) { Track.new(recordable_id: 1, recordable_type: "Guest", s3_string: "secret") }
 
   context "attributes" do
     it "has a recordable id" do
@@ -9,11 +12,19 @@ RSpec.describe Track, type: :model do
     end
 
     it "has a recordable type" do
-      expect(track.recordable_type).to eq "User"
+      expect(track.recordable_type).to eq "Guest"
     end
 
     it "has an s3 string" do
       expect(track.s3_string).to eq "secret"
     end
   end
+
+  context "associations" do
+    it "belongs to a guest" do
+      track.recordable = guest
+      expect(track.recordable).to be_instance_of(Guest)
+    end
+  end
+
 end
