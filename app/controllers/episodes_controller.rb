53819eeaf5189451c6996104ab58b1a6e5814020
Alex Raubach <antigrapist@gmail.com>
2017-02-25 11:12:59 -0600

    creates test route

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,9 +1,12 @@
 class EpisodesController < ApplicationController
   def show
-    @episode = Episode.find_by(:sharable_link, params[:sharable_link]) 
+    @episode = Episode.find_by(:sharable_link, params[:sharable_link])
   end
 
   def create
 
   end
+
+
+
 end
