2b17742742da3953b3c11be28654a409d1de5372
Oliver Duncan <oliverduncan@icloud.com>
2017-02-25 10:22:26 -0600

    Present modal for new episode

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,6 +31,10 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include Devise::Test::ControllerHelpers, :type => :controller
+  config.include Warden::Test::Helpers
+  config.after :each do
+    Warden.test_reset!
+  end
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
