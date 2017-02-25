fed0a1468a44f1e95801fd7b04d1af290e676b73
Oliver Duncan <oliverduncan@icloud.com>
2017-02-25 16:33:16 -0600

    Fix broken AWS method

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -24,7 +24,7 @@ class EpisodesController < ApplicationController
   private
 
   def set_s3_direct_post
-    @s3_direct_post = Rails.application.secrets.S3_BUCKET.presigned_post(
+    @s3_direct_post = S3_BUCKET.presigned_post(
     key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
   end
 
