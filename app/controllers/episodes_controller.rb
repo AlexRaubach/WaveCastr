725a0ed76e7a5ee5ad2ef9b761e7aafad80b343b
Mithridates01 <jclucas21@gmail.com>
2017-02-25 10:58:22 -0600

    added AWS S3 direct post method intol  controller and aws config class

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,9 +1,18 @@
 class EpisodesController < ApplicationController
+  before_action :set_s3_direct_post, only: [:create]
+
   def show
-    @episode = Episode.find_by(:sharable_link, params[:sharable_link]) 
+    @episode = Episode.find_by(:sharable_link, params[:sharable_link])
   end
 
   def create
 
   end
+
+  private
+  def set_s3_direct_post
+    @s3_direct_post = Rails.application.secrets.S3_BUCKET.presigned_post(
+    key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
+  end
+
 end
