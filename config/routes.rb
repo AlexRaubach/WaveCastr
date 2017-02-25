c47ac481a309761009d8f52f57182b2317d1fa79
Joshua Lucas <email": null>
2017-02-25 11:01:00 -0600

    create files, routes, view for testing AWS S3 welcome#show welcome#create

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-
-  root to: 'welcome#index'
-
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
-  
+
   devise_for :users
+
   resources :users, only: [:show]
+
   resources :episodes, only: [:create, :show]
 
+  resources :welcome, only: [:show, :create]
+
+  root to: 'welcome#index'
 end
