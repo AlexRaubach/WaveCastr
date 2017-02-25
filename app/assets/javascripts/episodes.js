071697ebb53aa0581414e100b50c1020a1d760d7
Mithridates01 <jclucas21@gmail.com>
2017-02-25 16:34:28 -0600

    moved aws s3 js

--- app/assets/javascripts/episodes.js
+++ app/assets/javascripts/episodes.js
@@ -1,37 +1,24 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-// $(document).ready(function() {
+$(function() {
+  $('.directUpload').find("input:file").each(function(i, elem) {
+    var fileInput    = $(elem);
+    var form         = $(fileInput.parents('form:first'));
+    var submitButton = form.find('input[type="submit"]');
+    var progressBar  = $("<div class='bar'></div>");
+    var barContainer = $("<div class='progress'></div>").append(progressBar);
+    fileInput.after(barContainer);
+    fileInput.fileupload({
+      fileInput:       fileInput,
+      url:             form.data('url'),
+      type:            'POST',
+      autoUpload:       true,
+      formData:         form.data('form-data'),
+      paramName:        'file', // S3 does not like nested name fields i.e. name="user[avatar_url]"
+      dataType:         'XML',  // S3 returns XML if success_action_status is set to 201
+      replaceFileInput: false
+    });
+  });
+});
 
-//   var form = new NewEpisodeForm('#new_episode');
-
-
-// });
-
-// function NewEpisodeForm(element) {
-//   this.element = element;
-//   this.bindSubmissionEvent();
-// }
-
-// NewEpisodeForm.prototype.bindSubmissionEvent = function() {
-//   $(this.element).on('submit', (e) => {
-//     e.preventDefault();
-//     this.presentModal();
-//   });
-// }
-
-// NewEpisodeForm.prototype.presentModal = function() {
-//   var $modal = this.modalTemplate();
-//   $modal.modal();
-// }
-
-// NewEpisodeForm.prototype.modalTemplate = function() {
-//   return `<div class="modal">
-//             <div class="modal-header">
-//               <h3>New Episode</h3>
-//             </div>
-//             <div class="modal-content'>
-//               <input name="episode['name']" placeholder="Episode name">  
-//             </div>
-//           </div>`
-// }
