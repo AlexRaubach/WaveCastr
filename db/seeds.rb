9504ab2934392aea2d79513bf33a330f08fb9f77
Oliver Duncan <oliverduncan@icloud.com>
2017-02-25 13:34:52 -0600

    Add username to users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(email: "oliverduncan@icloud.com", password: "password") 
-9.times { User.create(email: Faker::Internet.email, password: "password") }
+User.create(name: "ollieshmollie", email: "oliverduncan@icloud.com", password: "password") 
+9.times { User.create(name: Faker::Internet.user_name, email: Faker::Internet.email, password: "password") }
 
 User.all.each do |user|
   10.times { user.episodes.create(name: Faker::Book.title, description: Faker::Lorem.sentence) }
