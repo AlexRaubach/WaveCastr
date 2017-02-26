aec96b85bf0e1a10e4903afd014752e93a58540f
Joshua Lucas <email": null>
2017-02-25 20:39:42 -0600

    working s3 post file upload form; need to add link to database

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,4 +1,9 @@
 class Track < ApplicationRecord
   belongs_to :recordable, polymorphic: true
   validates :recordable_type, presence: true
+
+
+  def s3_link
+    S3_BUCKET.url.to_s + s3_string.to_s
+  end
 end
