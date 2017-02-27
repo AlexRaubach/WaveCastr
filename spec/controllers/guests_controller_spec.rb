a11da4eb8279c922aad9cc428f46e55be8770ec4
Oliver Duncan <oliverduncan@icloud.com>
2017-02-27 12:52:16 -0600

    Use selenium to test new guests

--- spec/controllers/guests_controller_spec.rb
+++ spec/controllers/guests_controller_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe GuestsController, type: :controller do
 
-  before(:each) do
-  end
+  let!(:user) { create(:user) }
+  let!(:episode) { user.episodes.create(attributes_for(:episode)) }
   
   it "returns 200 when creating new guest" do
-    post :create, params: { guest: attributes_for(:guest) } 
+    post :create, params: { guest: { name: "Bobo", episode_id: episode.id } }
     expect(response).to have_http_status :success
   end
 
