118e5aa42fd7d8971b522f92ecdc8bb7982f4c9f
jhyun94 <hyunjonathan@gmail.com>
2017-02-26 18:57:38 -0600

    gave start/start control to host only and some css

--- app/assets/javascripts/episodes.js
+++ app/assets/javascripts/episodes.js
@@ -34,4 +34,31 @@ $(function() {
   })
 });
 
+$(document).ready(function(){
+  $(".btn-group button").click(function(){
+    var choice = this.innerHTML;
+    $(".show-options").children().hide();
+    switchDiv(choice);
+
+  })
+
+})
+
+function switchDiv(choice){
+  switch (choice){
+    case "settings":
+      $(".settings").show();
+      break;
+    case "commands":
+      $(".commands").show();
+      break;
+    case "recordings":
+      $(".recordings").show();
+      break;
+    case "logs":
+      $(".logs").show();
+      break;
+  }
+}
+
 // }
