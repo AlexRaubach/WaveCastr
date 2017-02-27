9fb17d8de7f44434d78e760ae58de2f510bcdb25
jhyun94 <hyunjonathan@gmail.com>
2017-02-26 21:19:00 -0600

    got started on pubnub api

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,3 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+var pubnub = new PubNub({
+    subscribeKey: "pub-c-d4381d4f-440f-4eef-b35c-5bc6e23b2d78",
+    publishKey: "sub-c-d4379b4c-fc95-11e6-ba92-02ee2ddab7fe"
+})
+
+var box = PUBNUB.$('box'), input = PUBNUB.$('input'), channel = 'chat';
+debugger;
