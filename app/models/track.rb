6876768d82448b9e89ec754ecccd766903276ec3
Joshua Lucas <email": null>
2017-02-28 12:05:51 -0600

    changed audio file naming convention __guestname__

--- app/models/track.rb
+++ app/models/track.rb
@@ -3,7 +3,14 @@ class Track < ApplicationRecord
   belongs_to :episode
 
 
-  def s3_link
+  def url
     S3_BUCKET.url.to_s + s3_string.to_s
   end
+
+  def method_name
+
+  end
 end
+# all.each do |x|
+#   puts x.s3_string
+# end
