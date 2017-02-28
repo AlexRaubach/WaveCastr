25c95be037a61e7485183a5754bcbda645f20674
Joshua Lucas <email": null>
2017-02-28 12:41:06 -0600

    add virtual attributes of track; name and audio_format

--- app/models/track.rb
+++ app/models/track.rb
@@ -7,9 +7,15 @@ class Track < ApplicationRecord
     S3_BUCKET.url.to_s + s3_string.to_s
   end
 
-  def method_name
-
+  def name
+    s3_string.match(/\__(.*)\__/)[1]
   end
+
+  def audio_format
+    s3_string..match(/\.([0-9a-z]+)(?=[?#])|(?:[\w]+)$/)[0]
+  end
+
+
 end
 # all.each do |x|
 #   puts x.s3_string
