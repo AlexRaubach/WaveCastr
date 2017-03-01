9e50a066b09736018d1fe26abd58cb0838ab4282
Oliver Duncan <oliverduncan@icloud.com>
2017-03-01 11:58:20 -0600

    Raise a 404 if episode page not found

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,10 +1,10 @@
 class EpisodesController < ApplicationController
 
+  before_action :find_episode, only: [:show]
   before_action :authenticate_user!, only: [:create, :destroy]
 
   def show
     @chat_keys = [ENV['CHAT_SUBSCRIBE_KEY'], ENV['CHAT_PUBLISH_KEY']]
-    @episode = Episode.find_by(sharable_link: params[:sharable_link])
     set_s3_direct_post(@episode)
     @guest = Guest.new
   end
@@ -25,6 +25,11 @@ class EpisodesController < ApplicationController
   end
 
   private
+    def find_episode
+      @episode = Episode.find_by(sharable_link: params[:sharable_link]) 
+      @episode ? @episode : not_found
+    end
+
     def set_s3_direct_post(episode)
       @s3_direct_post = S3_BUCKET.presigned_post(
         key: "wavecastr/#{episode.host.name}/episode_#{episode.sharable_link}/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
