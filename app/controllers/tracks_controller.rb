aaa3e5bb5b4514cc7b04c7ad62fb0d356c608c84
Oliver Duncan <oliverduncan@icloud.com>
2017-03-01 15:58:15 -0600

    Add flash messages to possible error points

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -2,7 +2,11 @@ class TracksController < ApplicationController
   def create
     episode = Episode.find_by(sharable_link: params['sharable_link'])
     new_track = episode.tracks.build(track_params)
-    new_track.save ? head(:ok) : head(422)
+    if new_track.save
+      render(partial: '/layouts/notice', locals: { msg: "Recording was successfully saved.", name: "notice" }, status: 200)
+    else
+      render(partial: '/layouts/notice', locals: { msg: "Sorry, something went wrong. Please try again.", name: "alert" }, status: 422)
+    end
   end
 
   private
