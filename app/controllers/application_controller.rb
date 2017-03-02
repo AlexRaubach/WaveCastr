f2a60079ac53b3a4d13ca4439489e1a3117897ea
Alex Raubach <antigrapist@gmail.com>
2017-03-02 11:08:01 -0600

    fixes JS equality operator and unused resource

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     user_path(current_user)
   end
 
@@ -13,6 +13,6 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) 
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     end
 end
